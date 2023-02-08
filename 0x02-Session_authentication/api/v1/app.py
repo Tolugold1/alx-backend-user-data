23e58102ddb4bff24e2b9c3a4ecc54506fc51d14
Tolulope Adeleke <toluadelekesteve@gmail.com>
2023-02-08 12:54:35 -0800

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -59,8 +59,8 @@ def before_request() -> str:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    if auth.authorization_header(request)\
-        and auth.session_cookie(request):
+    if not auth.authorization_header(request)\
+        and not auth.session_cookie(request):
         abort(401)
     request.current_user = auth.current_user(request)
 
