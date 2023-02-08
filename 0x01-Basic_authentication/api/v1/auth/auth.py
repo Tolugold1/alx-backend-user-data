1732f9c2fa88a6f045d0e50241ae8ea8f930d3c8
Tolulope Adeleke <toluadelekesteve@gmail.com>
2023-02-08 03:43:36 -0800

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,11 +8,11 @@ class Auth:
     """class auth"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """authentication requirements"""
+        """ require authorithation """
         if path is None or excluded_paths is None or not len(excluded_paths):
             return True
-        if path[-1] == '/':
-            path += "/"
+        if path[-1] != '/':
+            path += '/'
         for i in excluded_paths:
             if i.endswith('*'):
                 if path.startswith(i[:1]):
@@ -20,7 +20,7 @@ class Auth:
         if path in excluded_paths:
             return False
         else:
-            return False
+            return True
 
     def authorization_header(self, request=request) -> str:
         """auth header"""
