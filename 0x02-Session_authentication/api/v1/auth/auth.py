502e670116832eaacabc4224280fbd84283a7460
Tolulope Adeleke <toluadelekesteve@gmail.com>
2023-02-08 11:16:58 -0800

    Update auth.py

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -22,9 +22,13 @@ class Auth:
         else:
             return True
 
-    def authorization_header(self, request=request) -> str:
-        """auth header"""
-        return None
+    def authorization_header(self, request=None) -> str:
+        """ authorization header """
+        if request is None:
+            return None
+        if not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=request) -> TypeVar('User'):
         """current user function/middleware"""
