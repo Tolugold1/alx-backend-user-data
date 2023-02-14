0a4e4a0f13fff0fa876676c9efc8397656d2c379
Tolugold <toluadelekesteve@gmail.com>
2023-02-14 21:11:09 +0100

    update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,10 +5,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"], strict_slashed=False)
-def message():
+def welcome_message():
     """return a json message"""
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
