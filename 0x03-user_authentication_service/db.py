ad98a7d4d2bbc6c7446f285498dcb4e41d830809
Tolugold <toluadelekesteve@gmail.com>
2023-02-14 19:01:15 +0100

    update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
-
+from typing import TypeVar
 from user import Base, User
 
 
