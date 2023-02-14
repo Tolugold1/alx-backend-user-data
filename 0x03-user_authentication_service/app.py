9197285dfd384149dd4283d4888af1ac5068af9b
Tolulope Adeleke <toluadelekesteve@gmail.com>
2023-02-14 12:58:36 -0800

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 """flask app"""
 from flask import Flask, jsonify, request, abort, redirect
-from sqlalchemy.orm.exc import NoResultFound
 from auth import Auth
 app = Flask(__name__)
-
-
 AUTH = Auth()
+
+
 @app.route("/", methods=["GET"], strict_slashes=False)
 def welcome_message():
     """return a json message"""
     return jsonify({"message": "Bienvenue"})
+
 
 @app.route("/users", methods=["POST"], strict_slashes=False)
 def users():
@@ -19,8 +19,7 @@ def users():
     pwd = request.form.get("password")
     try:
         AUTH.register_user(email, pwd)
-        return jsonify({"email": email,
-        "message": "user created"}), 200
+        return jsonify({"email": email, "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"}), 400
 
